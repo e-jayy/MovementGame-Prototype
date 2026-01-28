e6a1bce7226ad50554c67cf83eb4ece0dc8bd3d6
ejay <et4g23@soton.ac.uk>
2026-01-28 20:17:31 +0000

    Updated InputManager
    - Update input manager for each action
    - Along with PlayStation controller inputs

--- Movement Game Prototype/Assets/Scripts/InputManager.cs
+++ Movement Game Prototype/Assets/Scripts/InputManager.cs
@@ -9,9 +9,23 @@ public class InputManager : MonoBehaviour
 
     public bool MenuOpenCloseInput { get; private set; }
 
-    private PlayerInput _playerInput;
+    public Vector2 MoveInput { get; private set; }
+    public bool JumpJustPressed { get; private set; }
+    public bool JumpBeingHeld { get; private set; }
+    public bool JumpReleased { get; private set; }
+    public bool DashInput { get; private set; }
+    public bool HookInput { get; private set; }
+    public bool BounceInput { get; private set; }
+
 
     private InputAction _menuOpenCloseAction;
+
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+    private InputAction _dashAction;
+    private InputAction _hookAction;
+    private InputAction _bounceAction;
+    private PlayerInput _playerInput;
 
     private void Awake()
     {
@@ -21,11 +35,37 @@ public class InputManager : MonoBehaviour
         }
 
         _playerInput = GetComponent<PlayerInput>();
-        _menuOpenCloseAction = _playerInput.actions["MenuOpenClose"];
+
+        SetupInputActions();
     }
 
     private void Update()
     {
+        UpdateInputs();
+    }
+
+    private void SetupInputActions()
+    {
+        _moveAction = _playerInput.actions["Move"];
+        _jumpAction = _playerInput.actions["Jump"];
+        _dashAction = _playerInput.actions["Dash"];
+        _hookAction = _playerInput.actions["Hook"];
+        _bounceAction = _playerInput.actions["Bounce"];
+        _menuOpenCloseAction = _playerInput.actions["MenuOpenClose"];
+    }
+
+    private void UpdateInputs()
+    {
+        MoveInput = _moveAction.ReadValue<Vector2>();
+
+        JumpJustPressed = _jumpAction.WasPressedThisFrame();
+        JumpBeingHeld = _jumpAction.IsPressed();
+        JumpReleased = _jumpAction.WasReleasedThisFrame();
+
+        DashInput = _dashAction.WasPressedThisFrame();
+        HookInput = _hookAction.WasPressedThisFrame();
+        BounceInput = _bounceAction.WasPressedThisFrame();
+
         MenuOpenCloseInput = _menuOpenCloseAction.WasPressedThisFrame();
     }
 }
